40bace79fcebb67e4965eaac2edeaf71d7189b84
Your Name <you@example.com>
2022-10-23 23:09:03 +0545

    idk

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Rajeev Shrestha</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Hey it's me Rajeev Shrestha, an aspiring MERN developer. I use Javascript as my main language, but don't limit with it." />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
