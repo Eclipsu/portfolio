0f70db2c5714b2d7872e7092261353ae05e3bd96
Your Name <you@example.com>
2022-10-25 01:59:37 +0545

    A meta tag added for image and media querry for smaller screens

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,8 @@
-import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Head>
-        <title>Rajeev Shrestha</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Hey it's me Rajeev Shrestha, an aspiring MERN developer. I use Javascript as my main language, but don't limit with it." />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+import Head from "next/head";
 import Navbar from "../Components/Navigation/Navbar";
 import Profile from "../Components/Body/Profile";
 import Skills from "../Components/Body/Skills";
@@ -7,6 +8,13 @@ import Skills from "../Components/Body/Skills";
 export default function Home(props) {
   return (
     <div className="main">
+      <Head>
+        <title>Rajeev Shrestha</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Hey it's me Rajeev Shrestha, an aspiring MERN developer. I use Javascript as my main language, but don't limit with it." />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:image" content={props.image_url} />
+      </Head>
       <Navbar />
       <Profile image_url={props.image_url} />
       <Skills id="skills" />
